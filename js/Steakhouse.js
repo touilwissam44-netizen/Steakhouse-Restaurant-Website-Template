edb7fb543eaf7d2ac459ccb03fd8d63731019f68
touilwissam44-netizen <touilwissam44@gmail.com>
2026-03-10 17:38:06 +0000

    Add files via upload

---
+++ js/Steakhouse.js
@@ -0,0 +1,31 @@
+function Reservation(){
+    window.location.href = "reservation.html";
+}
+function Home(){
+    window.location.href = "index.html";
+}
+function About(){
+    window.location.href = "About.html";
+}
+function Events(){
+    window.location.href = "events.html";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
